2df5e4678bf1e76f92f11968875f0c809ac5d02c
Emilly Amorim <emillyamvrim@gmail.com>
2025-11-12 12:26:33 -0300

    jokenpo.py

--- jokenpo.py
+++ jokenpo.py
@@ -2,14 +2,20 @@
 
 import random
 from random import choice
+
 print("Bem vindo(a) ao jogo Jokenpô")
+nome_jogador = input("Digite o seu nome: ")
+
+contador_jogador = 0
+contador_maquina = 0
+
 while True:
     opcoes = ['pedra', 'papel', 'tesoura']
-    escolha = input("O que você vai jogar? \n" 
-    "    ☆ Pedra \n"
-    "    ☆ Papel \n"
-    "    ☆ Tesoura \n"
-    "    Qual sua escolha: ").strip().lower()
+    escolha = input("O que você vai jogar? \n"
+                    "    🪨 Pedra \n"
+                    "    📄 Papel \n"
+                    "    ✄ Tesoura \n"
+                    "    Qual sua escolha: ").strip().lower()
     if escolha not in opcoes:
         print("Essa não é uma opção valida, tente novamente inserindo uma das opções informadas")
         continue
@@ -17,7 +23,7 @@ while True:
         escolha_maquina = choice(opcoes)
         print(f'A maquina escolhe: {escolha_maquina}')
         if escolha == escolha_maquina:
-            escolha_empate = input("Vocês empataram! \n"
+            escolha_empate = input("🏁 Vocês empataram! \n"
                                    "deseja tentar novamente? (sim/não) ").strip().lower()
             if escolha_empate == "sim":
                 continue
@@ -27,16 +33,21 @@ while True:
 
         if escolha != escolha_maquina:
             if (escolha == "pedra" and escolha_maquina == "tesoura") or \
-            (escolha == "papel" and escolha_maquina == "pedra") or \
-            (escolha == "tesoura" and escolha_maquina == "papel"):
-                print("Parabéns!! Você venceu")
-            
+                    (escolha == "papel" and escolha_maquina == "pedra") or \
+                    (escolha == "tesoura" and escolha_maquina == "papel"):
+                print("🏆🎉 Parabéns!! Você venceu")
+                contador_jogador += 1
+
             else:
-                print("A maquina ganhou! sinto muito")
+                print("💔🥀 A maquina ganhou! sinto muito")
+                contador_maquina += 1
+
+            print("O placar atual esta:")
+            print(f'Maquina: {contador_maquina} X {nome_jogador}: {contador_jogador}')
 
             continuar_jogo2 = input("Você deseja continuar a jogar? (sim/não) ").strip().lower()
             if continuar_jogo2 == "sim":
-                 continue
+                continue
             else:
                 print("Foi um bom jogo! Até logo")
                 break
